5c07e7d3836e5e93ca9c37d9722571ab20ae3ccc
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-04-14 00:01:34 +0200

    Add project files.

---
+++ ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents;
+public class GridViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+    //invoked a partial view Views/Shared/Components/Grid/Default.cshtml
+        return View("Sample"); 
+    }
+}
